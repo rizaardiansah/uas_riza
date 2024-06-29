578594f2fa7880b257111a4343e49187881bbcff
unknown <saldutsalsa28@gmail.com>
2024-06-29 20:10:00 +0700

    UAS

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:qr_bar_code_scanner_dialog/qr_bar_code_scanner_dialog.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _qrBarCodeScannerDialogPlugin = QrBarCodeScannerDialog();
+  String? code;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('QR Scann app'),
+        ),
+        body: Builder(builder: (context) {
+          return Material(
+            child: Center(
+              child: ElevatedButton(
+                  onPressed: () {
+                    _qrBarCodeScannerDialogPlugin.getScannedQrBarCode(
+                        context: context,
+                        onCode: (code) {
+                          setState(() {
+                            this.code = code;
+                          });
+                        });
+                  },
+                  child: Text(code ?? "Click me")),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
